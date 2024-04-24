6cfd8b326f499fd4baf3cb0709fa3dd327e42c5d
summer words <82319973+hwhyyds@users.noreply.github.com>
2024-04-24 11:21:34 +0800

    Add files via upload

--- Webapp.py
+++ Webapp.py
@@ -5,13 +5,15 @@ import json
 import os
 
 app = Flask(__name__)
+model, tokenizer = load_model_and_tokenizer("./ChatGLM3/finetune_demo/output/checkpoint-30000")
 
 @app.route('/', methods=['GET'])
 def predict():
-    question = request.form.get('question')
-    id = request.form.get('id')
-    model, tokenizer = load_model_and_tokenizer("./ChatGLM3/finetune_demo/output/checkpoint-30000")
-    if id==2:
+    question = request.args.get('question')
+    print(question)
+    id = request.args.get('id')
+
+    if str(id) == '2':
         path="./ChatGLM3/finetune_demo/data/train.json"
     
         with open('./ChatGLM3/finetune_demo/data/train.json', 'r') as json_file:
@@ -22,23 +24,26 @@ def predict():
     
         distances, indices = search_similar_texts(question, k=1, path=path)
         rag_value = train_values[indices[0][0]]
-        print(os.getcwd())
         train_data_dic = get_dic_data(path=path)
         Q = list(train_data_dic.keys())[indices[0][0]]
         A = list(train_data_dic.values())[indices[0][0]]
-        new_prompt = question + f'\n你可以参考这个病案诊断进行回答 Q:{Q}\n:{A}'
+        new_prompt = f'这是一份资料Q:{Q}\n:{A}'+ '\n请根据上面这份资料，再结合下文患者的病案诊断下文患者的身体状况，并给出治法和方剂建议。' + question
         response, _ = model.chat(tokenizer, new_prompt)
+        send_data = {
+            'answer': response,
+            'is_rag': True,
+            'rag': f'Q:{Q}\nA:{A}'
+        }
     else:
+        print(question)
         response, _ = model.chat(tokenizer, question) #禁用RAG
     
-    send_data = {
-        'answer': response,
-        'id': id,
-        # 'is_rag': True,
-        # 'rag': f'{rag_value}'
-    }
+        send_data = {
+            'answer': response,
+            'is_rag': False,
+        }
     
-    return jsonify(send_data)
+    return send_data
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
